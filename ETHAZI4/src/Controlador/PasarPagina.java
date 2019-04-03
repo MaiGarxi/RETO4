66d7385261f441cde788510cd0ea13abd66aaf76
MaiGarxi <46573115+MaiGarxi@users.noreply.github.com>
2019-04-03 17:02:29 +0200

    crear pasarpagina

--- ETHAZI4/src/Controlador/PasarPagina.java
+++ ETHAZI4/src/Controlador/PasarPagina.java
@@ -1,6 +1,46 @@
 
 package Controlador;
 
+import Vista.Despedida;
+import Vista.Destino;
+import Vista.Lista;
+import Vista.Pago;
+
+
 public class PasarPagina {
     
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //FUNCION PARA PASAR DE BIENVENIDA A DESTINO
+    public static void classBienvenidaaDestino(){
+        //SE CREA EL OBJ DE LA CLASE DESTINO
+        Destino obj= new Destino();
+        obj.setVisible(true); 
+    } 
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //FUNCION PARA PASAR DE DESTINO A LISTA
+    public static void classDestinoaLista(){
+        //SE CREA EL OBJ DE LA CLASE LISTA
+        Lista obj= new Lista();
+        obj.setVisible(true); 
+    } 
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //FUNCION PARA PASAR DE DESTINO A LISTA
+    public static void classListaaDestino(){
+        Destino obj= new Destino();
+        obj.setVisible(true); 
+    } 
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //FUNCION PARA PASAR DE LISTA A PAGO
+    public static void classListaaPago(){
+        //SE CREA EL OBJ DE LA CLASE PAGO
+        Pago obj= new Pago();
+        obj.setVisible(true); 
+    } 
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //FUNCION PARA PASAR DE PAGO A DESPEDIDA
+    public static void classPagoaDespedida(){
+        //SE CREA EL OBJ DE LA CLASE DESPEDIDA
+        Despedida obj= new Despedida();
+        obj.setVisible(true); 
+    } 
 }
