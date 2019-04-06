8470bf9794ca8a24e841fd0da3158e77c2d225e7
dimaggio <noesvenezuela@gmail.com>
2019-04-06 15:52:46 +0200

    Txt Listo

--- ETHAZI4/src/Controlador/ControladorPago.java
+++ ETHAZI4/src/Controlador/ControladorPago.java
@@ -17,7 +17,7 @@ public class ControladorPago {
     public double moneda05=0, moneda02=0, moneda01=0, moneda005=0, moneda002=0, moneda001=0;
     public double valor, pago2;
 
-    public ControladorPago(ArrayList<reserva>reservas,JButton cancelar, JButton reiniciar, JButton confirmar,JLabel actualizaPago, JButton bi200, JButton bi100,JButton bi50, JButton bi20, JButton bi10, JButton bi5, JButton mo2, JButton mo1, JButton mo01, JButton mo02, JButton mo05, JButton mo001, JButton mo002, JButton mo005, JLabel totalAPagar,JList<String> lista) {
+    public ControladorPago(ArrayList<reserva>reservas,JButton cancelar, JButton reiniciar, JButton confirmar,JLabel actualizaPago, JButton bi200, JButton bi100,JButton bi50, JButton bi20, JButton bi10, JButton bi5, JButton mo2, JButton mo1, JButton mo01, JButton mo02, JButton mo05, JButton mo001, JButton mo002, JButton mo005, JLabel totalAPagar/*,JList<String> lista*/) {
         
         reserva reserv = new reserva();
         totalAPagar.setText(String.valueOf(reserv.calcular_total_pagar(reservas)+" €"));
@@ -47,6 +47,8 @@ public class ControladorPago {
                     for(int i=0; i<reservas.size();i++){
                         con.InsertarReserva(reservas.get(i).getPrecio(), reservas.get(i).getCod_hotel());
                     }
+                    reserva a= new reserva();
+                   System.out.println( a.crear_txt(reservas));
                 }
                 if (pago2>valor){
                     double cambio;
--- ETHAZI4/src/Modelo/reserva.java
+++ ETHAZI4/src/Modelo/reserva.java
@@ -1,6 +1,8 @@
 
 package Modelo;
 
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class reserva {
@@ -65,6 +67,38 @@ public class reserva {
    return precio_final;
    }
    
+   public String crear_txt(ArrayList<reserva> reservas)
+   {
+      
+          try{
+                 String ruta= "src\\modelo\\Reserva.txt"; 
+              FileWriter archivo = new FileWriter(ruta);
+           for(int x=0;x<reservas.size();x++)
+        {
+    
+          String mensaje =         " \n"
+				+"                                                              S.L.                        "+" \t\n"
+				+" **********************************************************************************************************************"+" \t\n"
+				
+				+" -Número de Reservas Realizadas"+(reservas.size())+"  \t \n\n"  
+				+" -Còdigo de Reserva : " +reservas.get(x).cod_reserva+" \t\t\n"
+				+" **********************************************************************************************************************"+" \t\n"
+				
+				+" -Còdigo del Hotel Reservado: " + reservas.get(x).cod_hotel+"  \t\n" 
+				+" **********************************************************************************************************************"+" \t\n"
+				
+				+" -Precio final: " +reservas.get(x).calcular_total_pagar(reservas)+" €";
+                                archivo.write(mensaje);
+   }
+           archivo.close();
+     return "Archivo Creado Con Exito";         
+   }catch(IOException e){
+       return  "Hubo un error";
+       
+   }
+          
+   }
    
+
     
 }
